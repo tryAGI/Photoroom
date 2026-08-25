f43dd706c2ad91000477968519e76e582a0c39c6
Konstantin S. <havendv@gmail.com>
2026-08-25 19:38:01 +0400

    feat: Updated OpenAPI spec (#56)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Photoroom/Generated/Photoroom.PhotoroomClient.VisualQa.g.cs
+++ src/libs/Photoroom/Generated/Photoroom.PhotoroomClient.VisualQa.g.cs
@@ -259,7 +259,7 @@ namespace Photoroom
                             {
 
                                 __httpRequestContent.Add(
-                                    content: new global::System.Net.Http.StringContent($"[{string.Join(",", global::System.Linq.Enumerable.Select(request.Features, x => x.ToValueString()))}]"),
+                                    content: new global::System.Net.Http.StringContent($"[{string.Join(",", global::System.Linq.Enumerable.Select(request.Features!, x => x.ToValueString()))}]"),
                                     name: "\"features\"");
 
                             }
